7106ecfc87a3585c82a96db03708d474194ab466
AndrewBerland <ajber6@gmail.com>
2026-04-09 22:24:32 -0400

    testing with values from a fake data set meant to mimick spacing of real data

--- frontend/components/MapView.tsx
+++ frontend/components/MapView.tsx
@@ -119,8 +119,8 @@ export default function MapView({
             "interpolate",
             ["linear"],
             ["zoom"],
-            3, 0.5,   
-            5.5, 0.6  
+            3, 0.3,   
+            5.5, 0.4  
           ],
 
           "heatmap-color": [
@@ -136,7 +136,7 @@ export default function MapView({
             "interpolate",
             ["exponential", 2],
             ["zoom"],
-            3, 2,
+            3, 2.5,
             5.5, 12 
           ],
           "heatmap-opacity": 0.8,
